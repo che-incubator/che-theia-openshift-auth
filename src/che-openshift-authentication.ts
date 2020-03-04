6c9c28afc510c62d2f64a5510f1097addb0a8b21
Igor Vinokur <ivinokur@redhat.com>
2020-03-04 16:26:20 +0200

    initial
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

---
+++ src/che-openshift-authentication.ts
@@ -0,0 +1,74 @@
+/*********************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import * as theia from '@theia/plugin';
+import * as che from '@eclipse-che/plugin';
+import { spawn } from 'child_process';
+
+export async function start(context: theia.PluginContext) {
+    if (! await isLoggedIn()) {
+        const action = await theia.window.showWarningMessage(`The OpenShift plugin is not authorized, would you like to authenticate?`, 'Yes', 'No');
+        if (action == 'Yes') {
+            let error = '';
+            const server = await getServerUrl();
+            const token = await che.openshift.getToken();
+            const osCommand = spawn('oc', ['login', server, '--certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', '--token', token]);
+            osCommand.stderr.on('data', data => {
+                error += data
+            });
+            osCommand.on('close', (code: number | null) => {
+                code = code === null ? 0 : code;
+                if (code === 0) {
+                    theia.window.showInformationMessage('OpenShift connector plugin successfully authenticated');
+                } else {
+                    theia.window.showErrorMessage('Failed to authenticated the OpenShift connector plugin: ' + error);
+                }
+            });
+        }
+    }
+    function isLoggedIn(): Promise<boolean> {
+        return new Promise<boolean>(resolve => {
+            let result = '';
+            const whoamiCommand = spawn('oc', ['whoami']);
+            whoamiCommand.stdout.on('data', data => {
+                result += data.toString();
+            });
+            whoamiCommand.stderr.on('data', () => {
+                resolve(false);
+            });
+            whoamiCommand.on('close', () => {
+                resolve(result != 'system:serviceaccount:che:che-workspace');
+            });
+        })
+    }
+
+    function getServerUrl(): Promise<string> {
+        return new Promise<string>(resolve => {
+            let result = '';
+            const versionCommand = spawn('odo', ['version']);
+            // tslint:disable-next-line:no-any
+            versionCommand.stdout.on('data', (data: any) => {
+                result += data.toString();
+            });
+            // tslint:disable-next-line:no-any
+            versionCommand.stderr.on('data', (data: any) => {
+                resolve('');
+            });
+            versionCommand.on('close', (code: number | null) => {
+                const server: string = result.substring(result.indexOf('Server: ') + 8, result.indexOf('Kubernetes: ') - 1);
+                resolve(server);
+            });
+        })
+    }
+}
+
+export function stop() {
+
+}
